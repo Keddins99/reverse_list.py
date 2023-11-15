452a5627a10f5a6e9a281a9703ff8f9c1d948d3d
Keddins <eddinskeathan@gmail.com>
2023-11-15 13:06:58 -0800

    reverse_list.py

--- reverse_list.py
+++ reverse_list.py
@@ -1,11 +1,11 @@
-def reverse_list(data_list):
-    i, j = 0, len(data_list) - 1
-    while i < j:
-        data_list[i], data_list[j] = data_list[j], data_list[i]
-        i += 1
-        j -= 1
+def reverse_list(lst):
+    length = len(lst)
 
+    for i in range(length // 2):
+        temp = lst[i]
+        lst[i] = lst[length - 1 - i]
+        lst[length - 1 - i] = temp
 
 vals = [7, -3, 12, 9]
 reverse_list(vals)
-print(vals)
+print(vals)  # This should print [9, 12, -3, 7]
